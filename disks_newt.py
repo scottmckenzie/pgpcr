612c256071105fd9c5617510681038945ed3813d
Jacob Adams <tookmund@gmail.com>
2018-05-14 16:19:26 -0400

    disks_newt: Display disk information via newt

---
+++ disks_newt.py
@@ -0,0 +1,10 @@
+from snack import *
+from . import disks
+
+def listdisks():
+	screen = SnackScreen()
+	d = disks.getdisks()
+	dlist = [x['model']+" "+x['size'] for x in d] #if x['tran'] == "usb"]
+	lcw = ListboxChoiceWindow(screen,"Disks", "Pick your disks", dlist)
+	screen.finish()
+	print(lcw)
