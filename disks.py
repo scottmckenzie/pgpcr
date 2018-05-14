91fa34f2dd28761dc897a8f5cad541809ac5ab8d
Jacob Adams <tookmund@gmail.com>
2018-05-14 16:42:04 -0400

    disks: Convert the bytes returned by subprocess to a string

--- disks.py
+++ disks.py
@@ -1,10 +1,10 @@
-import json
-import subprocess
+import json, sys, subprocess
 
 
 def getdisks():
 	p = subprocess.run(["lsblk", "-do", "name,size,type,mountpoint,tran,model", "--json"],stdout=subprocess.PIPE)
 	if p.returncode != 0:
 		return None
+	pstr = p.stdout.decode(sys.stdout.encoding)
 	j = json.loads(p.stdout)
 	return j['blockdevices']
