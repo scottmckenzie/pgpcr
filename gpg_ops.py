76a4e577280182d216d2c1b2254840d4ad036882
Jacob Adams <tookmund@gmail.com>
2018-05-15 10:56:33 -0400

    gpg_ops: Rewrite as GPGKey class

--- gpg_ops.py
+++ gpg_ops.py
@@ -1,19 +1,17 @@
 import gpg
 
-ctx = None
+class GPGKey:
 
-masteralgo = "rsa4096"
-subalgo = masteralgo
+	def __init__(self, home, progress):
+		self.ctx = gpg.Context(home_dir=home)
+		self.masteralgo = "rsa4096"
+		self.subalgo = "rsa2048"
+		self.ctx.set_progress_cb(progress)
 
-def init(home):
-	global ctx
-	ctx = gpg.Context(home_dir=home)
+	def genmaster(self, userid, password):
+		self.master = ctx.create_key(userid, algorithm=masteralgo, sign=True, certify=True, passphrase=password)
 
-def genmaster(userid, password):
-	return ctx.create_key(userid, algorithm=masteralgo, sign=True, certify=True, passphrase=password)
-
-def gensub(master, algo=subalgo):
-	s = ctx.create_subkey(master, algorithm=algo, sign=True)
-	e = ctx.create_subkey(master, algorithm=algo, encrypt=True)
-	a = ctx.create_subkey(master, algorithm=algo, authenticate=auth)
-	return (s, e, a)
+	def gensub(master, algo=self.subalgo):
+		self.subsig = ctx.create_subkey(master, algorithm=algo, sign=True)
+		self.subenc = ctx.create_subkey(master, algorithm=algo, encrypt=True)
+		self.subauth = ctx.create_subkey(master, algorithm=algo, authenticate=auth)
