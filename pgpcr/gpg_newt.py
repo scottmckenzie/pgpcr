dc6d1116fa0548fa038c100aae5d8f38c0ef1872
Jacob Adams <tookmund@gmail.com>
2018-05-16 18:50:00 -0400

    gpg_newt: Add approximate progress
    Not very accurate but better than nothing

--- pgpcr/gpg_newt.py
+++ pgpcr/gpg_newt.py
@@ -2,13 +2,20 @@ from snack import *
 from . import gpg_ops, common_newt as common
 
 def new(screen, workdir):
-	gk = gpg_ops.GPGKey(workdir.name, progress)
+	gk = gpg_ops.GPGKey(workdir.name)
 	ew = EntryWindow(screen, "New GPG Key", "Enter User Information", ["Name", "Email Address"])
 	name = ew[1][0]
 	email = ew[1][1]
 	pw = common.password(screen)
+	common.alert(screen, "Key Generation", "GPG keys will now be generated. Progress is estimated and this may take a while. You will be prompted for your password several times.")
+	mprog = common.Progress(screen, "Key Generation", "Generating Master Key...", 50)
+	gk.set_progress(progress, mprog)
 	gk.genmaster(name+" <"+email+">",pw)
+	sprog = common.Progress(screen, "Key Generation", "Generating Sub Keys...", 50)
+	gk.set_progress(progress, sprog)
 	gk.gensub()
+	common.alert(screen, "Key Generation", "Key Generation Complete!")
 
-def progress(what, type, current, total):
-	print(what, type, current, "/", total)
+def progress(what, type, current, total, prog):
+	if what == "primegen":
+		prog.inc(1)
