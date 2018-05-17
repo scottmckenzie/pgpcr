818b78b34f40e6502ab19ff45438c512d3ec8d11
Jacob Adams <tookmund@gmail.com>
2018-05-17 16:19:18 -0400

    common_newt: Add newt password callback

--- pgpcr/common_newt.py
+++ pgpcr/common_newt.py
@@ -3,7 +3,7 @@ from snack import *
 width = 40
 padding = (0, 0, 0, 1)
 
-def password(screen):
+def new_password(screen):
 	pass1 = Entry(20, password=1)
 	pass2 = Entry(20, password=1)
 	ew = EntryWindow(screen, "Password", "Enter your password", [("Password:", pass1), ("Password (again):", pass2)])
@@ -14,6 +14,15 @@ def password(screen):
 		error("Password cannot be empty!")
 		return password()
 	return pass1.value()
+
+def password(hint, desc, prev_bad, screen):
+	p = Entry(20, password=1)
+	label = "Password"
+	if prev_bad:
+		label += " (again)"
+	label += ":"
+	ew = EntryWindow(screen, hint, desc, [(label, p)], allowCancel=0)
+	return p.value()
 
 def alert(screen, title, msg):
 	ButtonChoiceWindow(screen, title, msg, ['Ok'])
