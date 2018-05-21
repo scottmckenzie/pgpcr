e504687f176c68fadaca06239bd6bac2687c12d5
Jacob Adams <tookmund@gmail.com>
2018-05-21 18:26:48 -0400

    disks_newt: loop if no removable storage connected

--- pgpcr/disks_newt.py
+++ pgpcr/disks_newt.py
@@ -1,8 +1,14 @@
 from snack import *
-from . import disks
+from . import disks, common_newt
+from time import sleep
 
 def listdisks(screen):
 	d = disks.getdisks()
+	if d == []:
+		common_newt.alert(screen, "Disks", "No removable storage connected. Please connect some and press OK.")
+		sleep(1)
+		listdisks(screen)
+		return
 	dlist = [x['model']+" "+x['size'] for x in d]
 	lcw = ListboxChoiceWindow(screen,"Disks", "Pick your disks", dlist)
 	print(lcw)
