f97499b157a0f71948c5100250c84c83ac99e6fa
Jacob Adams <tookmund@gmail.com>
2018-05-22 16:11:57 -0400

    disks: Add mount and format functions

--- pgpcr/disks.py
+++ pgpcr/disks.py
@@ -1,4 +1,4 @@
-import json, sys, subprocess
+import json, sys, subprocess, os
 
 def getdisks():
 	p = subprocess.run(["lsblk", "-p", "-d", "-o", "tran,name,model,size,serial,mountpoint", "--json"], stdout=subprocess.PIPE)
@@ -8,4 +8,16 @@ def getdisks():
 	j = json.loads(p.stdout)
 	return [x for x in j['blockdevices'] if x['tran'] == "usb"]
 
+def format(device):
+	ret = subprocess.run(["sudo", "pgpcr-part", device['name']])
+	return ret.returncode
 
+def mount(device):
+	mountdir = "/mnt/"+device['serial']
+	try:
+		os.mkdir(mountdir)
+	except FileExistsError:
+		pass # Just need it to exist, don't need to create it
+	ret = subprocess.run(["sudo", "mount", device['name']+"1", mountdir])
+	if ret == 0:
+		device['mountpoint'] = mountdir
