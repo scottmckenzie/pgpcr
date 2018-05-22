e7a36f765aeadeb7625175ac9e117a83e6895504
Jacob Adams <tookmund@gmail.com>
2018-05-22 15:15:19 -0400

    disks: clarify lsblk usage and remove unneeded information

--- pgpcr/disks.py
+++ pgpcr/disks.py
@@ -1,10 +1,11 @@
 import json, sys, subprocess
 
-
 def getdisks():
-	p = subprocess.run(["lsblk", "-do", "name,size,type,mountpoint,tran,model", "--json"],stdout=subprocess.PIPE)
+	p = subprocess.run(["lsblk", "-d", "-o", "name,size,mountpoint,tran,model", "--json"], stdout=subprocess.PIPE)
 	if p.returncode != 0:
 		return None
 	pstr = p.stdout.decode(sys.stdout.encoding)
 	j = json.loads(p.stdout)
 	return [x for x in j['blockdevices'] if x['tran'] == "usb"]
+
+
