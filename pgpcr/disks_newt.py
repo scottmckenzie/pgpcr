706cf0308a23715e9cfa1e633764141587a1eb62
Jacob Adams <tookmund@gmail.com>
2018-05-22 17:34:37 -0400

    disks_newt: update listdisks to accept a use string and return the chosen disk

--- pgpcr/disks_newt.py
+++ pgpcr/disks_newt.py
@@ -2,16 +2,21 @@ from snack import *
 from . import disks, common_newt
 from time import sleep
 
-def listdisks(screen):
+def listdisks(screen, use):
 	d = disks.getdisks()
 	if d == []:
 		common_newt.alert(screen, "Disks", "No removable storage connected. Please connect some and press OK.")
 		sleep(1)
 		listdisks(screen)
 		return
-	dlist = [x['model']+" "+x['size'] for x in d]
-	lcw = ListboxChoiceWindow(screen,"Disks", "Pick your disks", dlist)
-	print(lcw)
+	for x in d:
+		x['displayname'] = x['model']+" "+x['size']
+	dlist = [x['displayname'] for x in d]
+	lcw = ListboxChoiceWindow(screen,"Disks", "Pick your "+use+" disk", dlist)
+	if lcw[0] is None or lcw[0] == 'ok':
+		return d[lcw[1]]
+	else:
+		return None
 
 def store(screen, workdir):
 	pass
