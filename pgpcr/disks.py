bff0e60ea02c655c06dd2e0942072b5c12bbecbe
Jacob Adams <tookmund@gmail.com>
2018-05-23 18:16:35 -0400

    disks: Add checkmounted and lsblk helper functions

--- pgpcr/disks.py
+++ pgpcr/disks.py
@@ -1,12 +1,8 @@
 import json, sys, subprocess, shutil, os
 
 def getdisks():
-	p = subprocess.run(["lsblk", "-p", "-d", "-o", "tran,name,model,size,serial,mountpoint", "--json"], stdout=subprocess.PIPE)
-	if p.returncode != 0:
-		return None
-	pstr = p.stdout.decode(sys.stdout.encoding)
-	j = json.loads(pstr)
-	return [x for x in j['blockdevices'] if x['tran'] == "usb"]
+	j = lsblk(["-p", "-d", "-o", "tran,name,model,size,serial,mountpoint", "--json"])
+	return [x for x in j if x['tran'] == "usb"]
 
 def setup(device):
 	# TODO: Remove skeleton code
@@ -24,3 +20,21 @@ def setup(device):
 
 def backup(workdir, destdir, name):
 	return shutil.copytree(workdir.name, destdir+"/"+name)
+
+# check if the given device has any mounted child devices
+def checkmounted(device):
+	j = lsblk(["-p", "-o", "name,mountpoint", device])
+	if "children" not in j:
+		return False
+	for x in j["children"]:
+		if x['mountpoint'] is not None:
+			return True
+	return False
+
+def lsblk(options):
+	com = ["lsblk"].append(options)
+	p = subprocess.run(com, stdout=subprocess.PIPE)
+	if p.returncode != 0:
+		return None
+	pstr = p.stdout.decode(sys.stdout.encoding)
+	return json.loads(pstr)['blockdevices']
