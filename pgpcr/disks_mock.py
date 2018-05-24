5e3a8dc6922206807574fd22455507ee4d03bc79
Jacob Adams <tookmund@gmail.com>
2018-05-24 16:52:32 -0400

    disk_mount: Use disk objects and /tmp/ directories as the mountpoint

--- pgpcr/disks_mock.py
+++ pgpcr/disks_mock.py
@@ -4,7 +4,7 @@ import json, sys, subprocess, shutil, os
 def getdisks():
 	d = []
 	for x in range(3):
-		d.append({"name": "/tmp/test"+str(x), "tran": "usb", "model": "TEST"+str(x), "serial":str(x), "mountpoint": None, "size": "1G"})
+		d.append(Disk({"name": "/tmp/test"+str(x), "model": "TEST"+str(x), "serial":str(x), "size": "1G"}))
 	return d
 
 class Disk:
@@ -25,8 +25,8 @@ class Disk:
 		return False
 
 	def setup(self):
-		os.mkdir(self.name)
-		self.mountpoint = self.name
+		os.mkdir(self.path)
+		self.mountpoint = self.path
 		return (True, True)
 
 	def backup(self, workdir, name):
