0367b381d4d6f1f3b147234115ef198c2f99d10f
Jacob Adams <tookmund@gmail.com>
2018-05-23 20:02:28 -0400

    test_disks_newt: test store

--- tests/test_disks_newt.py
+++ tests/test_disks_newt.py
@@ -1,12 +1,19 @@
 from pgpcr import disks_newt
-import unittest, snack
+import unittest, snack, tempfile
 
 class testDisksNewt(unittest.TestCase):
 	def setUp(self):
 		self.screen = snack.SnackScreen()
+		self.tmp = tempfile.TemporaryDirectory()
+		with open(self.tmp.name+"/testfile", "w") as f:
+			f.write("this is a test. please ignore")
 
 	def tearDown(self):
 		self.screen.finish()
+		self.tmp.cleanup()
 
 	def test_pickdisks(self):
-		disks_newt.pickdisks(self.screen, "TEST")
+		disks_newt.pickdisks(self.screen, "PICKDISKS TEST")
+
+	def test_store(self):
+		disks_newt.store(self.screen, self.tmp, "STORETEST")
