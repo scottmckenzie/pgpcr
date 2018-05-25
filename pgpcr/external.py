45e822d12dc965868b842782382178b5300da664
Jacob Adams <tookmund@gmail.com>
2018-05-25 15:20:17 -0400

    external: use keyword dictionary correctly

--- pgpcr/external.py
+++ pgpcr/external.py
@@ -2,10 +2,13 @@ from subprocess import *
 import sys
 
 def process(cmd):
-	args = {check: True, stdout: PIPE, stderr: PIPE}j
+	args = {'check': True, 'stdout': PIPE, 'stderr': PIPE}
 	if sys.version_info.minor >= 6:
-		args.encoding = sys.stdout.encoding
+		args['encoding'] = sys.stdout.encoding
 	ret = run(cmd, **args)
 	if sys.version_info.minor <= 5:
-		ret.stdout = ret.stdout.decode(sys.stdout.encoding)
+		if ret.stdout is not None:
+			ret.stdout = ret.stdout.decode(sys.stdout.encoding)
+		if ret.stderr is not None:
+			ret.stderr = ret.stderr.decode(sys.stderr.encoding)
 	return ret
