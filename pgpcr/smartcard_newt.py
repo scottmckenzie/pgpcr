d5c54b854d1964db802e629188ad1bf5f809d7bd
Jacob Adams <tookmund@gmail.com>
2018-06-01 13:29:36 -0400

    smartcard_newt: initial export skeleton

--- pgpcr/smartcard_newt.py
+++ pgpcr/smartcard_newt.py
@@ -1,5 +1,21 @@
 from snack import *
-from . import disks_newt
+from . import smartcard
 
-def export(screen, workdir):
-	pass
+def pickcard(screen):
+	s = smartcard.getsmartcards()
+	if s == []:
+		common.alert("Smartcards", "No smartcards detected. Please connect one and press Ok.")
+		sleep(1)
+		return pickcard(screen)
+	slist = [str(x) for x in s]
+	lcw = ListboxChoiceWindow(screen, "Smartcards", "Pick your smartcard", slist, buttons = [("Refresh", "refresh")])
+	if lcw[0] is None or lcw[0] == 'ok':
+		return s[lcw[1]]
+	elif lcw[0] == "refresh":
+		return pickcard(screen)
+	else:
+		return None
+
+def export(screen, gk):
+	smart = pickcard(screen)
+	# TODO: Read over assuan commands used by GPA and figure out keytocard
