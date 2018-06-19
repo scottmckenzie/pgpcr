0e75fc44eb7fdddef99d335d8a7aef7c358ff456
Jacob Adams <tookmund@gmail.com>
2018-06-19 18:35:51 -0400

    smartcard_newt: Rewrite pickcard as gpg only detects one at a time

--- pgpcr/smartcard_newt.py
+++ pgpcr/smartcard_newt.py
@@ -3,22 +3,18 @@ from . import smartcard, common_newt as common
 
 
 def pickcard(screen):
-    s = smartcard.getsmartcards()
-    if s == []:
+    s = smartcard.getsmartcard()
+    if s is None:
         common.alert(_("Smartcards"),
                      _("No smartcards detected."
                        " Please connect one and press Ok."))
         sleep(1)
         return pickcard(screen)
-    slist = [str(x) for x in s]
-    lcw = ListboxChoiceWindow(screen, _("Smartcards"), _("Pick your smartcard"),
-                              slist, buttons=[(_("Refresh"), "refresh")])
-    if lcw[0] is None or lcw[0] == "ok":
-        return s[lcw[1]]
-    elif lcw[0] == "refresh":
-        return pickcard(screen)
-    else:
-        return None
+    card = common.confirm(screen, _("Smartcard"), _("Is this your card?")+"\n"
+            str(s))
+    if card:
+        return s
+    return pickcard(screen)
 
 
 def export(screen, gk):
