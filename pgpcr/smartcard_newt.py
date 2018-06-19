2af40034bde9c1607b4aa29d7ec4bae184c6a4e7
Jacob Adams <tookmund@gmail.com>
2018-06-19 18:36:49 -0400

    smartcard_newt: Export keys to smartcard

--- pgpcr/smartcard_newt.py
+++ pgpcr/smartcard_newt.py
@@ -19,8 +19,28 @@ def pickcard(screen):
 
 def export(screen, gk):
     smart = pickcard(screen)
-    # TODO: Read over assuan commands used by GPA and figure out keytocard
-
+    keys = gk.keys
+    ccw = common.CheckboxChoiceWindow(screen, gk.fpr,
+                                      _("Which key(s) do you want to export?"),
+                                      keys)
+    if ccw[0] == "cancel":
+        return
+    for k in ccw[1]:
+        lcw = ListboxChoiceWindow(screen, k, _("Which slot do you want to put"
+            " this key in?"), smart.slots, buttons = [(_("Ok"), "ok"),
+            (_("Cancel"), "cancel")])
+        if lcw[0] == "cancel":
+            continue
+        slot = lcw[1]+1
+        fpr = k.split(" ")[0]
+        try:
+            gk.keytocard(fpr, slot)
+        except gpg_interact.OverwriteError:
+            overwrite = common.dangerConfirm(screen, _("Overwrite?"),
+                _("There is already a key in slot %d. Do you want to overwrite"
+                "it?"))
+            if overwrite:
+                gk.keytocard(fpr, slot, True)
 
 def generate(screen, workdir):
     common.NotImplementedYet(screen)
