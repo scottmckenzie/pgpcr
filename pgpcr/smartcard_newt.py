64e35e9eaa14cf400757fb3f88b2c055cebd39b4
Jacob Adams <tookmund@gmail.com>
2018-06-20 15:01:22 -0400

    smartcard_newt: Add new smartcard setup

--- pgpcr/smartcard_newt.py
+++ pgpcr/smartcard_newt.py
@@ -20,6 +20,11 @@ def pickcard(screen, gk):
 
 def export(screen, gk):
     smart = pickcard(screen, gk)
+    if smart.new:
+        ns = common.confirm(screen, _("New Smartcard"), _("This appears to be"
+        " a new smartcard. Would you like to set it up?"))
+        if ns:
+            setup(screen, smart)
     keys = gk.keys
     ccw = common.CheckboxChoiceWindow(screen, gk.fpr,
                                       _("Which key(s) do you want to export?"),
@@ -42,6 +47,20 @@ def export(screen, gk):
                 "it?"))
             if overwrite:
                 gk.keytocard(fpr, slot, True)
+
+def setup(screen, smart):
+    ew = EntryWindow(screen, _("New Smartcard"), _("Setup your new smartcard"),
+            [_("Name of cardholder"), _("Language Preference"),
+                _("Sex (m/f/u)"), _("Login Data")],
+            buttons = [(_("Ok"), "ok"), (_("Cancel"),
+                "cancel")])
+    if ew[0] == "cancel":
+        return
+    #TODO: Name may need to be internationalized
+    smart.name = ew[1][0].split(" ")
+    smart.lang = ew[1][1]
+    smart.sex = ew[1][2]
+    smart.login = ew[1][3]
 
 def generate(screen, workdir):
     common.NotImplementedYet(screen)
