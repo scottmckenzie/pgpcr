8e1be2ab4a4e1a21b03d2379bf3aeb1300b08f77
Jacob Adams <tookmund@gmail.com>
2018-06-28 12:13:53 -0400

    test_smartcard: Add smartcard tests

---
+++ tests/test_smartcard.py
@@ -0,0 +1,26 @@
+import unittest
+from os import environ
+from pgpcr.smartcard import *
+
+@unittest.skipUnless("PGPCRINTERACT" in environ.keys(), "Interactive tests"
+        " disabled. Set PGPCRINTERACT to enable them")
+class SmartcardTest(unittest.TestCase):
+    def setUp(self):
+        self.smart = Smartcard()
+
+    def test_properties(self):
+        print(self.smart.name)
+        print(self.smart.login)
+        print(self.smart.sex)
+        print(self.smart.reader)
+        print(self.smart.vendor)
+        print(self.smart.serial)
+
+    def test_set_properties(self):
+        self.smart.name = "Test Name"
+        print(self.smart.name)
+        self.smart.url = "https://example.com"
+        print(self.smart.url)
+
+    def test_set_PIN(self):
+        self.smart.setPIN()
