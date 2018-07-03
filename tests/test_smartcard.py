d19686f18c1a817f51b81b16dfeff0645bd977f2
Jacob Adams <tookmund@gmail.com>
2018-07-03 19:16:28 -0400

    test_smartcard: Test setting sex and login properties

--- tests/test_smartcard.py
+++ tests/test_smartcard.py
@@ -13,8 +13,8 @@ class SmartcardTest(unittest.TestCase):
     def test_properties(self):
         print()
         print(self.smart.name)
-        print(self.smart.login)
         print(self.smart.sex)
+        print(self.smart.login)
         print(self.smart.reader)
         print(self.smart.vendor)
         print(self.smart.serial)
@@ -25,6 +25,10 @@ class SmartcardTest(unittest.TestCase):
         print(self.smart.name)
         self.smart.url = "https://example.com"
         print(self.smart.url)
+        self.smart.sex = "m"
+        print(self.smart.sex)
+        self.smart.login = "login"
+        print(self.smart.login)
 
     def test_set_PIN(self):
         self.smart.setPIN()
