242104adfdeccc076bdad2c8f0bac964372148a7
Jacob Adams <tookmund@gmail.com>
2018-07-05 13:31:50 -0400

    smartcard: Add support for Yubikey settouch

--- pgpcr/smartcard.py
+++ pgpcr/smartcard.py
@@ -15,6 +15,8 @@ class OverwriteError(Exception):
     pass
 class SocketNotFound(Exception):
     pass
+class UnsupportedOperation(Exception):
+    pass
 class SmartcardError(Exception):
     def __init__(self, msg=""):
         self.msg = msg
@@ -167,6 +169,28 @@ class Smartcard:
 
     def setAdminPIN(self):
         self._scd("PASSWD 3")
+
+    # Based on yubitouch.sh https://github.com/a-dma/yubitouch
+    def yk_settouch(operation, requirement):
+        if self.vendor != "Yubico":
+            raise UnsupportedOperation
+        if operation == "sig":
+            DO = "D6"
+        elif operation == "dec":
+            DO = "D7"
+        elif operation == "aut":
+            DO = "D8"
+        else:
+            raise UnsupportedOperation
+        if requirement == False:
+            UIF = "00"
+        if requirement == True:
+            UIF = "01"
+        if requirement == None:
+            UIF = "02"
+        else:
+            raise UnsupportedOperation
+        self._scd("APDU 00 da 00 "+DO+" 02 "+UIF+" 20")
 
 
 sexopt = {"m": 1, "f": 2, "u": 9}
