ca378f4067c146bbb3839320db70f6885a95cfc5
Jacob Adams <tookmund@gmail.com>
2018-07-17 15:26:50 -0400

    pgp-clean-room, tran: Support default language and s/inittran/init/g

--- pgpcr/tran.py
+++ pgpcr/tran.py
@@ -1,8 +1,10 @@
 import gettext
 
-def inittran(lang):
-    tran = gettext.translation("pgpcr", "/usr/share/locale/", [lang],
-        fallback=True)
+def init(lang=None):
+    ll = None
+    if lang is not None:
+        ll = [lang]
+    tran = gettext.translation("pgpcr", languages=ll, fallback=True)
     tran.install()
     if type(tran) is gettext.NullTranslations and lang != "en":
         return False
