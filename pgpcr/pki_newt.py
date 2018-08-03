b53ec75e7994470569d766a79adabdbefe3cacd4
Jacob Adams <tookmund@gmail.com>
2018-08-03 15:51:00 -0400

    pki_newt: Use ca dictionaries for better CA setup

--- pgpcr/pki_newt.py
+++ pgpcr/pki_newt.py
@@ -2,28 +2,39 @@ from . import newt
 from . import ca
 from . import disks_newt
 from . import external
+from . import time
 
 def new(workdir):
     screen = newt.Screen()
     ew = newt.EW(screen, _("New CA"), _("Fill out the fields below to create"
         " a new Certificate Authority."), [
             _("Name of CA"),
-            _("Days CA should be valid for"),
+            _("When the CA should expire (YYYY-MM-DD)"),
             _("Domain of CA"),
-            _("Days Server certificates should be valid for"),
-            _("Key Type"),
-            _("Key Size"),
-            _("Certificate Digest")])
+            _("Days Server certificates should be valid for")])
     if ew[0] == "cancel":
         return
     CA = ca.CA(workdir)
     CA.name = ew[1][0]
-    CA.CAValid = ew[1][1]
+    delta = time.isostr2delta(ew[1][1])
+    CA.CAValid = delta.days
     CA.domain = ew[1][2]
     CA.serverValid = ew[1][3]
-    CA.keyType = ew[1][4]
-    CA.keySize = ew[1][5]
-    CA.digest = ew[1][6]
+    kt = list(ca.keyTypes.keys())
+    ktlcw = newt.LCW(screen, _("CA Key Type"), _("Pick your key type"), kt)
+    if ktlcw[0]:
+        return
+    CA.keyType = kt[ktlcw[1]]
+    ks = list(ca.keyTypes[CA.keyType])
+    kslcw = newt.LCW(screen, _("CA Key Size"), _("Pick your key size"), ks)
+    if kslcw[0]:
+        return
+    CA.keySize = ks[kslcw[1]]
+    digestlcw = newt.LCW(screen, _("Certificate Digest Type"), _("Pick your"
+        " digest type"), ca.digests)
+    if digestlcw[0]:
+        return
+    CA.digest = ca.digests[digestlcw[1]]
 
     CA.save()
     try:
