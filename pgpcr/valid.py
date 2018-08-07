ef7e3f97fbca9322b03502aca4b86cdfa2ad30ea
Jacob Adams <tookmund@gmail.com>
2018-08-07 16:35:42 -0400

    valid: Add domain validation

--- pgpcr/valid.py
+++ pgpcr/valid.py
@@ -3,6 +3,8 @@ import re
 # http://emailregex.com/
 _email = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 _date = re.compile(r"(\d{4})-(\d{2})-(\d{2})")
+# https://stackoverflow.com/a/20204811
+_domain = re.compile(r"(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)")
 
 def _matchregex(r, s):
     if r.match(s) is not None:
@@ -13,5 +15,8 @@ def _matchregex(r, s):
 def email(address):
     return _matchregex(_email, address)
 
-def date(d):
-    return _matchregex(_date, d)
+def date(date):
+    return _matchregex(_date, date)
+
+def domain(domain):
+    return _matchregex(_domain, domain)
